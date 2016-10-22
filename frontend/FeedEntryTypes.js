846f30a7b4c8cb90e2aadbb1bc1f3343f0a0ccdf
Rolf Erik Lekang <me@rolflekang.com>
2016-10-22 18:40:59 +0200

    fix: Add urlPlaceholder texts to entry types

--- frontend/FeedEntryTypes.js
+++ frontend/FeedEntryTypes.js
@@ -3,26 +3,31 @@ const FeedEntryTypes = [
     key: 'WORKING_ON_ISSUE',
     description: 'A github issue you wish to fix',
     feedText: 'is working on',
+    urlPlaceholder: 'URL to issue',
   },
   {
     key: 'CREATED_ISSUE',
-    description: 'A github issue you created',
+    description: 'A github issue you have created',
     feedText: 'created an issue on',
+    urlPlaceholder: 'URL to issue',
   },
   {
     key: 'CODING_REPOSITORIY',
     description: 'The project repo you wish to send a request to (if no issue exists)',
     feedText: 'is working on',
+    urlPlaceholder: 'URL to repository',
   },
   {
     key: 'TRIAGING_REPOSITORY',
-    description: 'Your pull request',
+    description: 'Triaging issues on repo',
     feedText: 'is triaging issues on',
+    urlPlaceholder: 'URL to repository',
   },
   {
     key: 'CREATED_PR',
     description: 'Your pull request',
     feedText: 'created a PR on',
+    urlPlaceholder: 'URL to pull-request',
   },
 ];
 
